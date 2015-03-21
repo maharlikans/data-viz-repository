ade9ee0722e4a24e2950184e308957a246bf92ee
Kyle Maharlika <kyle.maharlika@gmail.com>
2015-03-21 12:20:32 -0400

    Initialized project with yeoman.io

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dataVizRepositoryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dataVizRepositoryApp
+ */
+angular.module('dataVizRepositoryApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
