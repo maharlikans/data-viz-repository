458a9b633c892f6ee1fef7049aa29d4dfa7f5ad5
Kyle Maharlika <kyle.maharlika@gmail.com>
2015-03-23 18:48:29 -0400

    Added more videos to hardcoded list

--- app/scripts/controllers/videos.js
+++ app/scripts/controllers/videos.js
@@ -11,6 +11,21 @@ angular.module('dataVizRepositoryApp')
   .controller('VideosCtrl', function ($scope) {
     $scope.videos = [
       {
+        title: 'Aaron Koblin: Artfully visualizing our humanity',
+        url: 'https://youtu.be/4v4XxlfVk3o',
+        embed: '<iframe width="560" height="315" src="https://www.youtube.com/embed/4v4XxlfVk3o" frameborder="0" allowfullscreen></iframe>'
+      },
+      {
+        title: 'Nathalie Miebach: Art made of storms',
+        url: 'https://youtu.be/MbhNaj88uL4',
+        embed: '<iframe width="560" height="315" src="https://www.youtube.com/embed/MbhNaj88uL4" frameborder="0" allowfullscreen></iframe>'
+      },
+      {
+        title: 'Enactive Mandala - Audio-visualizing Brain Waves',
+        url: 'https://youtu.be/1Y0BNSbOwQs',
+        embed: '<iframe width="560" height="315" src="https://www.youtube.com/embed/1Y0BNSbOwQs" frameborder="0" allowfullscreen></iframe>'
+      },
+      {
         title: 'Partitura 001 from Abstract Birds',
         url: 'https://vimeo.com/abstractbirds/partitura001',
         embed: '<iframe src="https://player.vimeo.com/video/23316783?color=ffffff&byline=0" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/23316783">Partitura 001</a> from <a href="https://vimeo.com/abstractbirds">Abstract Birds</a> on <a href="https://vimeo.com">Vimeo</a>.</p>'
