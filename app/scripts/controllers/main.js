1371351239fd9e63508d047873c95ba27b8e8551
Kyle Maharlika <kyle.maharlika@gmail.com>
2015-03-23 15:13:10 -0400

    Updated width of web page to 960px max

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,9 +11,11 @@ angular.module('dataVizRepositoryApp')
   .controller('MainCtrl', function ($scope) {
     $scope.active = 1;
     $scope.nav = ["Home", "Links", "Papers", "Videos"];
+
     $scope.isNavActive = function(isNavActive) {
       return isNavActive === $scope.active;
     };
+
     $scope.setNavActive = function(setNavActive) {
       $scope.active = setNavActive;
     };
